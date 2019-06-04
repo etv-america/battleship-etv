d9bf3a21723c978866df6b2d3d8e170034ca4e51
LEONHOME1500 <asl1513@hotmail.com>
2019-06-04 10:19:54 -0700

    Added more to welcome screen and reformatted code

--- battleship.py
+++ battleship.py
@@ -24,10 +24,12 @@ def check_vic(tally):
 
 
 def print_welcome():
-    print("Welcome To Battle Ship")
+    print("Welcome To Battle Ship! The game of luck and strategy.")
     response = input("Would you like to play a game? Y/N")
     if response is "N":
         exit()
+
+
 def print_exit():
     pass
 
